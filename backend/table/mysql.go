67810d444804a5e78476f78876d0eea72c0d6e3e
siou <siou@sioudeMacBook-Air.local>
2022-10-18 19:14:38 +0800

    fixed weektype to uppercase

--- backend/table/mysql.go
+++ backend/table/mysql.go
@@ -150,7 +150,7 @@ type BanchRule struct {
 	MinPeople int `json:"MinPeople"` // 限制最少員工
 	MaxPeople int `json:"MaxPeople"` // 限制做多的員工
 	WeekDay int `json:"WeekDay"` // 星期幾 (1, 2, 3, 4, 5, 6, 7)
-	WeekType int `json:"weekType"` // 寒暑假 或 平常(1, 2, 3)
+	WeekType int `json:"WeekType"` // 寒暑假 或 平常(1, 2, 3)
 	OnShiftTime string  `json:"OnShiftTime"`// 開始上班時間
 	OffShiftTime string `json:"OffShiftTime"` //結束上班的時間
 	CreateTime time.Time `json:"CreateTime"`//創建的時間
