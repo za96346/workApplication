c99518e010467e9ea5807330a78e4666eb8b5c0d
siou <siou@sioudeMacBook-Air.local>
2022-11-05 17:51:27 +0800

    fixed wait reply struct

--- backend/response/type.go
+++ backend/response/type.go
@@ -69,3 +69,14 @@ type Shift struct {
 	// OffShiftTime time.Time `json:"OffShiftTime"` //結束上班的時間
 
 }
+
+type WaitReply struct {
+    WaitId     int64     `json:"WaitId"`     // 等待的唯一id
+    UserId     int64     `json:"UserId"`     // 使用者的編號
+	UserName   string    `json:"UserName"`   // 名字
+    CompanyId  int64     `json:"CompanyId"`  // 公司碼
+    SpecifyTag string    `json:"SpecifyTag"` // 備註
+    IsAccept   int       `json:"IsAccept"`   // 是否接受 (1 等待確認, 2 接受, 3 拒絕)
+    CreateTime time.Time `json:"CreateTime"` //創建的時間
+    LastModify time.Time `json:"LastModify"` // 上次修改的時間
+}
--- backend/service/companyService.go
+++ backend/service/companyService.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"backend/methods"
+	"backend/response"
 	"backend/table"
 	"net/http"
 	"sync"
@@ -738,8 +739,24 @@ func FetchWaitReply (props *gin.Context, waitJob *sync.WaitGroup) {
 	if err {return}
 
 	waitCompanyReply := (*dbHandle).SelectWaitCompanyReply(3, company.CompanyId)
+	res := []response.WaitReply{}
+	for _, v := range *waitCompanyReply {
+		findUser := (*dbHandle).SelectUser(1, v.UserId)
+		if !methods.IsNotExited(findUser) {
+			res = append(res, response.WaitReply{
+				UserName: (*findUser)[0].UserName,
+				WaitId: v.WaitId,
+				UserId: v.UserId,
+				CompanyId: v.CompanyId,
+				SpecifyTag: v.SpecifyTag,
+				IsAccept: v.IsAccept,
+				CreateTime: v.CreateTime,
+				LastModify: v.LastModify,
+			})
+		}
+	}
 	props.JSON(http.StatusOK, gin.H{
-		"data": *waitCompanyReply,
+		"data": res,
 		"message": StatusText().FindSuccess,
 	})
 }
