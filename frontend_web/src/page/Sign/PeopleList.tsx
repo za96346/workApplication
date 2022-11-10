57fdbaf561cdf1d583d7ae45a4d1c3db3cd3467b
siou <siou@sioudeMacBook-Air.local>
2022-11-10 15:47:39 +0800

    splite isaccept state

---
+++ frontend_web/src/page/Sign/PeopleList.tsx
@@ -0,0 +1,39 @@
+import { Avatar, Button, List } from 'antd'
+import React from 'react'
+import useReduceing from '../../Hook/useReducing'
+import statics from '../../statics'
+import { WaitReplyType } from '../../type'
+interface props {
+    onConfirm: (a: WaitReplyType['WaitId'], isAccept: WaitReplyType['IsAccept']) => Promise<void>
+    keys: WaitReplyType['IsAccept']
+}
+const PeopleList = ({ onConfirm, keys }: props): JSX.Element => {
+    const { company } = useReduceing()
+    const waitReply = company.waitReply.filter((item) => item.IsAccept === keys)
+    return (
+        <List
+            itemLayout="horizontal"
+            dataSource={waitReply}
+            renderItem={item => (
+                <List.Item style={{ flexWrap: 'wrap', position: 'relative' }}>
+                    <List.Item.Meta
+                        style={{ minWidth: '200px', marginBottom: '30px' }}
+                        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                        title={<>{item.UserName}</>}
+                        description={<>{statics.isAccept[item?.IsAccept || 1]}</>}
+                    />
+                    <div style={{ position: 'absolute', right: '0px', bottom: '5px' }}>
+                        <Button
+                            onClick={async () => await onConfirm(item.WaitId, 2)}
+                            style={{ backgroundColor: 'skyblue', color: 'white' }}
+                        >
+                                確認
+                        </Button>
+                        <Button onClick={async () => await onConfirm(item.WaitId, 3)}>拒絕</Button>
+                    </div>
+                </List.Item>
+            )}
+        />
+    )
+}
+export default PeopleList
--- frontend_web/src/page/Sign/Sign.tsx
+++ frontend_web/src/page/Sign/Sign.tsx
@@ -1,48 +1,37 @@
-import { Avatar, Button, List } from 'antd'
+import { Tabs } from 'antd'
 import React, { useEffect } from 'react'
 import api from '../../api/api'
-import useReduceing from '../../Hook/useReducing'
-import statics from '../../statics'
 import { WaitReplyType } from '../../type'
+import PeopleList from './PeopleList'
 
 const SignPage = (): JSX.Element => {
-    const { company } = useReduceing()
-    const waitReply = company.waitReply
-    const onConfirm = async (WaitId: WaitReplyType['WaitId']): Promise<void> => {
-        await api.updateWaitReply({
+    const onConfirm = async (WaitId: WaitReplyType['WaitId'], isAccept: WaitReplyType['IsAccept']): Promise<void> => {
+        const res = await api.updateWaitReply({
             SpecifyTag: '',
-            IsAccept: 2,
+            IsAccept: isAccept,
             WaitId
         })
+        if (res.status) {
+            await api.getWaitReply()
+        }
     }
     useEffect(() => {
         api.getWaitReply()
     }, [])
     return (
+
         <div className={styles.signBlock}>
-            <List
-                itemLayout="horizontal"
-                dataSource={waitReply}
-                renderItem={item => (
-                    <List.Item style={{ flexWrap: 'wrap', position: 'relative' }}>
-                        <List.Item.Meta
-                            style={{ minWidth: '200px', marginBottom: '30px' }}
-                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                            title={<>{item.UserName}</>}
-                            description={<>{statics.isAccept[item?.IsAccept || 1]}</>}
-                        />
-                        <div style={{ position: 'absolute', right: '0px', bottom: '5px' }}>
-                            <Button
-                                onClick={async () => await onConfirm(item.WaitId)}
-                                style={{ backgroundColor: 'skyblue', color: 'white' }}
-                            >
-                                確認
-                            </Button>
-                            <Button>拒絕</Button>
-                        </div>
-                    </List.Item>
-                )}
-            />
+            <Tabs>
+                <Tabs.TabPane tab={'申請中'} key={0}>
+                    <PeopleList keys={1} onConfirm={onConfirm}/>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={'拒絕'} key={1}>
+                    <PeopleList keys={3} onConfirm={onConfirm}/>
+                </Tabs.TabPane>
+                <Tabs.TabPane tab={'接受'} key={2}>
+                    <PeopleList keys={2} onConfirm={onConfirm}/>
+                </Tabs.TabPane>
+            </Tabs>
         </div>
     )
 }
