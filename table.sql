cc111e149ad501d859d50f217351b7f8aa8b41f5
siou <za96346@gmail.com>
2022-12-21 22:38:34 +0800

    fix table

--- table.sql
+++ table.sql
@@ -3,29 +3,29 @@ use workApplication;
 # 使用者
 create table user(
 	userId bigint not null AUTO_INCREMENT unique,
-	companyCode varchar(50),
-	userName varchar(20),
-	employeeNumber varchar(30),
+	companyCode varchar(50) default '',
+	userName varchar(20) default '',
+	employeeNumber varchar(30) default '',
 	account varchar(50) primary key,
-	password varchar(50),
-	onWorkDay timestamp,
-	banch bigint,
-	permession int,
-	createTime timestamp,
-	lastModify timestamp,
-	monthSalary int,
-	partTimeSalary int
+	password varchar(50) default '',
+	onWorkDay timestamp default now(),
+	banch bigint default -1,
+	permession int default 2,
+	createTime timestamp default now(),
+	lastModify timestamp default now(),
+	monthSalary int default 0,
+	partTimeSalary int default 0
 );
 alter table user auto_increment=1;
 
 # 使用者偏好
 create table userPreference(
 	userId bigint primary key,
-	style varchar(50),
-	fontSize varchar(3),
-	selfPhoto blob,
-	createTime timestamp,
-	lastModify timestamp
+	style varchar(50) default '',
+	fontSize varchar(3) default '',
+	selfPhoto blob default null,
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table userPreference add foreign key(userId) references user(userId) on update cascade on delete cascade;
 
@@ -33,15 +33,15 @@ alter table userPreference add foreign key(userId) references user(userId) on up
 create table shift(
 	shiftId bigint not null AUTO_INCREMENT unique,
 	userId bigint,
-	banchStyleId bigint,
+	banchStyleId bigint default -1,
 	onShiftTime timestamp,
 	offShiftTime timestamp,
 	restTime time,
-	punchIn timestamp,
-	punchOut timestamp,
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	punchIn timestamp default null,
+	punchOut timestamp default null,
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table shift add primary key (onShiftTime, offShiftTime, userId, restTime);
 alter table shift add foreign key(userId) references user(userId) on update cascade on delete cascade;
@@ -52,11 +52,11 @@ create table shiftChange(
 	caseId bigint not null unique auto_increment,
 	initiatorShiftId bigint,
 	requestedShiftId bigint,
-	reason varchar(200),
-	caseProcess varchar(10),
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	reason varchar(200) default '',
+	caseProcess varchar(10) default '',
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table shiftChange add primary key (initiatorShiftId, requestedShiftId);
 alter table shiftChange add foreign key(initiatorShiftId) references shift(shiftId) on update cascade on delete cascade;
@@ -68,11 +68,11 @@ create table shiftOverTime(
 	shiftId bigint,
 	initiatorOnOverTime timestamp,
 	initiatorOffOverTime timestamp,
-	reason varchar(200),
-	caseProcess varchar(10),
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	reason varchar(200) default '',
+	caseProcess varchar(10) default '',
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table shiftOverTime add primary key (shiftId, initiatorOnOverTime, initiatorOffOverTime);
 alter table shiftOverTime add foreign key (shiftId) references shift(shiftId) on update cascade on delete cascade;
@@ -82,11 +82,11 @@ create table forgetPunch(
 	caseId bigint not null unique auto_increment,
 	shiftId bigint,
 	targetPunch varchar(3),
-	reason varchar(200),
-	caseProcess varchar(10),
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	reason varchar(200) default '',
+	caseProcess varchar(10) default '',
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table forgetPunch add primary key (shiftId, targetPunch);
 alter table forgetPunch add foreign key (shiftId) references shift(shiftId) on update cascade on delete cascade;
@@ -96,11 +96,11 @@ create table dayOff(
 	caseId bigint not null unique auto_increment,
 	shiftId bigint,
 	dayOffType varchar(10),
-	reason varchar(200),
-	caseProcess varchar(10),
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	reason varchar(200) default '',
+	caseProcess varchar(10) default '',
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table dayOff add primary key (shiftId, dayOffType);
 alter table dayOff add foreign key (shiftId) references shift(shiftId) on update cascade on delete cascade;
@@ -110,11 +110,11 @@ create table lateExcused(
 	caseId bigint not null unique auto_increment,
 	shiftId bigint,
 	lateExcusedType varchar(10),
-	reason varchar(200),
-	caseProcess varchar(10),
-	specifyTag varchar(50),
-	createTime timestamp,
-	lastModify timestamp
+	reason varchar(200) default '',
+	caseProcess varchar(10) default '',
+	specifyTag varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table lateExcused add primary key (shiftId, lateExcusedType);
 alter table lateExcused add foreign key (shiftId) references shift(shiftId) on update cascade on delete cascade;
@@ -123,15 +123,15 @@ alter table lateExcused add foreign key (shiftId) references shift(shiftId) on u
 create table company(
 	companyId bigint not null unique auto_increment,
 	companyCode varchar(50) unique,
-	companyName varchar(200),
-	companyLocation varchar(200),
-	companyPhoneNumber varchar(20),
-	bossId bigint not null,
-	settlementDate int,
-	termStart timestamp,
-	termEnd timestamp,
-	createTime timestamp,
-	lastModify timestamp
+	companyName varchar(200) default '',
+	companyLocation varchar(200) default '',
+	companyPhoneNumber varchar(20) default '',
+	bossId bigint not null default -1,
+	settlementDate int default -1,
+	termStart timestamp default now(),
+	termEnd timestamp default now(),
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table company add primary key (companyId, companyCode);
 alter table company auto_increment=1;
@@ -141,9 +141,9 @@ create table companyBanch(
 	id bigint not null unique auto_increment,
 	companyId bigint,
 	banchName varchar(50),
-	banchShiftStyle varchar(200),
-	createTime timestamp,
-	lastModify timestamp
+	banchShiftStyle varchar(200) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table companyBanch add primary key (companyId, banchName);
 alter table companyBanch add foreign key (companyId) references company(companyId) on update cascade on delete cascade;
@@ -154,11 +154,11 @@ create table banchStyle(
 	banchId bigint,
 	icon varchar(100),
 	restTime time,
-	timeRangeName varchar(20),
+	timeRangeName varchar(20) default '',
 	onShiftTime time,
 	offShiftTime time,
-	createTime timestamp,
-	lastModify timestamp
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table banchStyle auto_increment=1;
 alter table banchStyle add primary key(banchId, restTime, onShiftTime, offShiftTime, icon);
@@ -186,16 +186,16 @@ create table quitWorkUser(
 	quitId bigint not null unique auto_increment,
 	userId bigint,
 	companyCode varchar(20),
-	userName varchar(20),
-	employeeNumber varchar(30),
-	account varchar(50),
-	onWorkDay timestamp,
-	banch bigint,
-	permession int,
-	monthSalary int,
-	partTimeSalary int,
-	createTime timestamp,
-	lastModify timestamp
+	userName varchar(20) default '',
+	employeeNumber varchar(30) default '',
+	account varchar(50) default '',
+	onWorkDay timestamp default now(),
+	banch bigint default -1,
+	permession int default 2,
+	monthSalary int default 0,
+	partTimeSalary int default 0,
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table quitWorkUser auto_increment=1;
 alter table quitWorkUser add primary key (userId, companyCode);
@@ -207,10 +207,10 @@ create table waitCompanyReply(
 	waitId bigint not null unique auto_increment,
 	userId bigint,
 	companyId bigint,
-	specifyTag varchar(50),
-	isAccept int,
-	createTime timestamp,
-	lastModify timestamp
+	specifyTag varchar(50) default '',
+	isAccept int default 1,
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table waitCompanyReply auto_increment=1;
 alter table waitCompanyReply add primary key (userId, companyId);
@@ -235,11 +235,11 @@ create table workTIme(
 	userId bigInt,
 	year int,
 	month int,
-	workHours int,
-	timeOff int,
-	usePaidVocation int,
-	createTime timestamp,
-	lastModify timestamp
+	workHours int default 8,
+	timeOff int default 0,
+	usePaidVocation int default 0,
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table workTime add primary key(`userId`, `year`, `month`);
 alter table workTime add foreign key(`userId`) references user(`userId`) on update cascade on delete cascade;
@@ -250,9 +250,9 @@ create table paidVocation(
 	paidVocationId bigInt not null unique auto_increment,
 	userId bigInt,
 	year int,
-	count int,
-	createTime timestamp,
-	lastModify timestamp
+	count int default 0,
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table paidVocation add primary key(`userId`, `year`);
 alter table paidVocation add foreign key(`userId`) references user(`userId`) on update cascade on delete cascade;
@@ -264,15 +264,17 @@ create table performance(
 	userId bigInt not null,
 	year int,
 	month int,
-	banchId bigInt,
-	goal varchar(1000),
-	attitude int,
-	efficiency int,
-	professional int,
-	directions varchar(1000),
-	beLate int,
-	dayOffNotOnRule int,
-	banchName varchar(50)        
+	banchId bigInt default -1,
+	goal varchar(1000) default '',
+	attitude int default 0,
+	efficiency int default 0,
+	professional int default 0,
+	directions varchar(1000) default '',
+	beLate int default 0,
+	dayOffNotOnRule int default 0,
+	banchName varchar(50) default '',
+	createTime timestamp default now(),
+	lastModify timestamp default now()
 );
 alter table performance add primary key(`userId`, `year`, `month`);
 alter table performance add foreign key(`userId`) references user(`userId`) on update cascade on delete cascade;
