cd4cf757bba112f4524c2de270d117d49314b3fb
siou <za96346@gmail.com>
2023-01-01 16:16:27 +0800

    fix

--- frontend_web/src/page/Setting/component/describe/Personal.tsx
+++ frontend_web/src/page/Setting/component/describe/Personal.tsx
@@ -5,6 +5,7 @@ import useReduceing from 'Hook/useReducing'
 // import { Button as MainBtn } from '../../../../component/Button'
 
 import statics from '../../../../statics'
+import dateHandle from 'method/dateHandle'
 
 const PersonalDescibe = (): JSX.Element => {
     const { loading, user } = useReduceing()
@@ -19,7 +20,7 @@ const PersonalDescibe = (): JSX.Element => {
                 <Descriptions.Item label="帳號" span={2}>{user.selfData.Account}</Descriptions.Item>
                 <Descriptions.Item label="姓名" span={2}>{user.selfData.UserName}</Descriptions.Item>
                 <Descriptions.Item label="公司編號" span={2}>{user.selfData.CompanyCode}</Descriptions.Item>
-                <Descriptions.Item label="到職日" span={2}>{`${user.selfData?.OnWorkDay}`}</Descriptions.Item>
+                <Descriptions.Item label="到職日" span={2}>{`${dateHandle.transferUtcFormat(user.selfData?.OnWorkDay)}`}</Descriptions.Item>
                 <Descriptions.Item label="部門" span={2}>{user.selfData?.BanchName}</Descriptions.Item>
                 <Descriptions.Item label="職位" span={2}>
                     {statics.permession[user.selfData.Permession]}
