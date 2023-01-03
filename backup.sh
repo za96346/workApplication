de9493c21bfdf9e330394b2e860b43e12ff5fa5d
siou <za96346@gmail.com>
2023-01-03 20:37:51 +0800

    add backup

---
+++ backup.sh
@@ -0,0 +1,5 @@
+CONTAINER=workapp_mysql
+DB_NAME=workApplication
+FILENAME=/backup/workApplication/${DB_NAME}_$(date "+%Y%m%d_%H%M%S").sql
+
+docker exec ${CONTAINER} sh -c "mysqldump -u root ${DB_NAME}" > ${FILENAME}
