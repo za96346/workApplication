691009aa4b6b9b82d07e1d65166aae45840ccf77
siou <za96346@gmail.com>
2023-01-03 23:14:27 +0800

    fix backup

--- backup.sh
+++ backup.sh
@@ -4,12 +4,12 @@ DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
 set timeout 10s
 expect << EOF
-spawn docker exec -i ${CONTAINER} sh -c "mysqldump -u root -p ${DB_NAME}" > ${FILENAME}
+spawn docker exec -i $CONTAINER sh -c "mysqldump -u root -p $DB_NAME" > $FILENAME
 expect {
     "*Enter password:*" {
         send "siou0722\r"
+        send "exit\r"
     }
 }
 expect eof
 EOF
-echo "success" >&2
