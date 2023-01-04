70bec3df2fe2d25846143dfa1f0297f415eea7b8
siou <za96346@gmail.com>
2023-01-04 21:15:43 +0800

    fix backup

--- backup.sh
+++ backup.sh
@@ -2,9 +2,8 @@
 CONTAINER=workapp_mysql
 DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
-echo "" > $FILENAME
 expect <<EOF
-spawn docker exec -i $CONTAINER bash -c "mysqldump -u root -p $DB_NAME" > $FILENAME
+spawn echo "fuck you" > $FILENAME
 expect {
     "*Enter password*" {
         send "siou0722\r"
