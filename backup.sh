16b0350173eec17b69d1608885b5cd3848b38bbc
siou <za96346@gmail.com>
2023-01-04 22:12:10 +0800

    fix backup

--- backup.sh
+++ backup.sh
@@ -2,4 +2,5 @@
 CONTAINER=workapp_mysql
 DB_NAME=workApplication
 FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
-docker exec -i $CONTAINER bash -c "mysqldump -u root --password siou0722 -p $DB_NAME" > $FILENAME
+pwd=siou0722
+docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME
