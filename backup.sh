e37bf42ea23a977248dce621a50b8196417d409e
siou <za96346@gmail.com>
2023-01-06 21:08:51 +0800

    removed backup

--- backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CONTAINER=workapp_mysql
-DB_NAME=workApplication
-FILENAME=~/backup/workApplication/"$(date "+%Y%m%d_%H%M%S")".sql
-pwd=siou0722
-docker exec -i $CONTAINER bash -c "mysqldump -u root -p$pwd  $DB_NAME" > $FILENAME
