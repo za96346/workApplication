45aac22044a58cc476c36af4df8f5631acf81d45
za96346 <za96346@gmail.com>
2023-11-06 16:28:00 +0800

    fixed test sql

--- Untitled.sql
+++ Untitled.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.29, for macos12 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: new_workapp
+-- Host: localhost    Database: new_workapp
 -- ------------------------------------------------------
--- Server version	8.2.0
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -41,7 +41,37 @@ CREATE TABLE `company` (
 
 LOCK TABLES `company` WRITE;
 /*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` VALUES (0,'ddd','ddd','wjiefoijwi','0906930873',5,'2023-11-06 15:11:34','2023-11-06 15:23:14');
 /*!40000 ALTER TABLE `company` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `company_banch`
+--
+
+DROP TABLE IF EXISTS `company_banch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `company_banch` (
+  `companyId` int NOT NULL COMMENT '公司Id',
+  `banchId` int NOT NULL COMMENT '部門id',
+  `banchName` varchar(50) NOT NULL COMMENT '部門名稱',
+  `deleteFlag` char(1) DEFAULT 'N' COMMENT '刪除旗標 ( N, Y )',
+  `deleteTime` datetime DEFAULT NULL COMMENT '刪除時間',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '創建時間',
+  `lastModify` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '最後更新時間',
+  PRIMARY KEY (`companyId`,`banchId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='公司部門';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `company_banch`
+--
+
+LOCK TABLES `company_banch` WRITE;
+/*!40000 ALTER TABLE `company_banch` DISABLE KEYS */;
+INSERT INTO `company_banch` VALUES (0,1,'資訊組','N',NULL,'2023-11-04 17:14:03','2023-11-04 17:14:03'),(0,2,'輔導組','N',NULL,'2023-11-04 17:14:03','2023-11-04 17:14:03'),(0,3,'研發部部','Y','2023-11-06 16:21:50','2023-11-06 16:14:49','2023-11-06 16:21:50');
+/*!40000 ALTER TABLE `company_banch` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,6 +86,8 @@ CREATE TABLE `function_item` (
   `funcName` varchar(30) DEFAULT NULL COMMENT '功能名稱',
   `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '創建時間',
   `lastModify` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '最近修改',
+  `scopeRoleEnable` char(1) DEFAULT 'Y' COMMENT '可編輯角色範圍',
+  `scopeBanchEnable` char(1) DEFAULT 'Y' COMMENT '可編輯部門範圍',
   PRIMARY KEY (`funcCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='功能項目;';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -66,7 +98,7 @@ CREATE TABLE `function_item` (
 
 LOCK TABLES `function_item` WRITE;
 /*!40000 ALTER TABLE `function_item` DISABLE KEYS */;
-INSERT INTO `function_item` VALUES ('banchManage','部門管理','2023-11-02 14:48:33','2023-11-02 14:48:33'),('companyData','公司基本資料','2023-11-02 14:47:56','2023-11-02 14:47:56'),('employeeManage','員工管理','2023-11-02 14:49:06','2023-11-02 14:49:06'),('performance','績效管理','2023-11-02 14:50:03','2023-11-02 14:50:03'),('selfData','基本資料','2023-11-02 14:47:03','2023-11-02 14:47:03'),('shift','排班管理','2023-11-02 14:51:32','2023-11-02 14:51:32'),('shiftSetting','班表設定','2023-11-02 14:51:01','2023-11-02 14:51:01'),('yearPerformance','年度績效管理','2023-11-02 14:50:03','2023-11-02 14:50:03');
+INSERT INTO `function_item` VALUES ('banchManage','部門管理','2023-11-02 14:48:33','2023-11-02 14:48:33','N','Y'),('companyData','公司基本資料','2023-11-02 14:47:56','2023-11-02 14:47:56','N','N'),('employeeManage','員工管理','2023-11-02 14:49:06','2023-11-02 14:49:06','Y','Y'),('performance','績效管理','2023-11-02 14:50:03','2023-11-02 14:50:03','Y','Y'),('roleManage','角色管理','2023-11-04 14:55:50','2023-11-04 14:55:50','N','N'),('selfData','基本資料','2023-11-02 14:47:03','2023-11-02 14:47:03','N','N'),('shift','排班管理','2023-11-02 14:51:32','2023-11-02 14:51:32','Y','Y'),('shiftSetting','班表設定','2023-11-02 14:51:01','2023-11-02 14:51:01','N','Y'),('yearPerformance','年度績效管理','2023-11-02 14:50:03','2023-11-02 14:50:03','Y','Y');
 /*!40000 ALTER TABLE `function_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +154,7 @@ CREATE TABLE `role` (
 
 LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (0,1,'admin','N','N',NULL,'2023-11-02 19:53:02','2023-11-02 19:53:02'),(0,2,'admin','N','N',NULL,'2023-11-02 19:53:16','2023-11-02 21:04:16');
+INSERT INTO `role` VALUES (0,1,'admin','N','N',NULL,'2023-11-02 19:53:02','2023-11-06 16:10:49'),(0,2,'admin','N','N',NULL,'2023-11-02 19:53:16','2023-11-04 13:42:08');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -138,6 +170,7 @@ CREATE TABLE `role_struct` (
   `roleId` int NOT NULL COMMENT '角色id',
   `funcCode` varchar(255) NOT NULL COMMENT '功能代碼( banchManage, shiftedit )',
   `itemCode` varchar(255) NOT NULL COMMENT '操作代碼(edit, delete...)',
+  `scopeBanch` text COMMENT '可操作部門範圍 ( 部門ID[] )',
   `scopeRole` text COMMENT '可操作角色範圍 ( 角色ID[] )',
   `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '創建時間',
   `lastModify` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '最近修改',
@@ -151,7 +184,7 @@ CREATE TABLE `role_struct` (
 
 LOCK TABLES `role_struct` WRITE;
 /*!40000 ALTER TABLE `role_struct` DISABLE KEYS */;
-INSERT INTO `role_struct` VALUES (0,2,'banchManage','delete','[]','2023-11-02 21:04:16','2023-11-02 21:04:16'),(0,2,'banchManage','edit','[]','2023-11-02 21:04:16','2023-11-02 21:04:16'),(0,2,'selfData','delete','[]','2023-11-02 21:04:16','2023-11-02 21:04:16'),(0,2,'selfData','edit','[]','2023-11-02 21:04:16','2023-11-02 21:04:16'),(1,1,'banchManage','delete','[]','2023-11-02 17:16:29','2023-11-02 17:16:29'),(1,1,'banchManage','edit','[]','2023-11-02 17:16:29','2023-11-02 17:16:29'),(1,1,'selfData','delete','[]','2023-11-02 17:16:29','2023-11-02 17:16:29'),(1,1,'selfData','edit','[]','2023-11-02 17:16:29','2023-11-02 17:16:29'),(1,2,'banchManage','delete','[]','2023-11-02 17:50:38','2023-11-02 17:50:38'),(1,2,'banchManage','edit','[]','2023-11-02 17:50:38','2023-11-02 17:50:38'),(1,2,'selfData','delete','[]','2023-11-02 17:50:38','2023-11-02 17:50:38'),(1,2,'selfData','edit','[]','2023-11-02 17:50:38','2023-11-02 17:50:38');
+INSERT INTO `role_struct` VALUES (0,1,'banchManage','add','self','[1,2]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'banchManage','delete','all','[1,2,3]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'banchManage','edit','all','[1,2,3]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'banchManage','inquire','self','[1,2]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'companyData','add','self','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'companyData','delete','all','[1,2,3]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'companyData','edit','all','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'companyData','inquire','self','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'employeeManage','add','self','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'employeeManage','delete','all','[1,2,3]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'employeeManage','edit','all','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'employeeManage','inquire','self','self','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'selfData','delete','all','all','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,1,'selfData','edit','all','[1,2,3]','2023-11-06 16:10:49','2023-11-06 16:10:49'),(0,2,'banchManage','delete','All','[1,2,3]','2023-11-04 13:42:08','2023-11-04 13:42:08'),(0,2,'banchManage','edit','All','[1,2,3]','2023-11-04 13:42:08','2023-11-04 13:42:08'),(0,2,'selfData','delete','All','All','2023-11-04 13:42:08','2023-11-04 13:42:08'),(0,2,'selfData','edit','All','[1,2,3]','2023-11-04 13:42:08','2023-11-04 13:42:08');
 /*!40000 ALTER TABLE `role_struct` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -186,6 +219,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('0',1,1,1,'SIOU','0906930873','za96346','aa20010722','2023-11-04 16:31:18','N',NULL,'2023-11-04 16:31:18','2023-11-04 16:31:18'),('0',2,2,1,'a00001','a00001','a00001','a00001','2023-11-04 16:31:56','N',NULL,'2023-11-04 16:31:56','2023-11-04 16:31:56'),('0',3,1,1,'黃明修00','za999','za999','aa20010722','2006-01-02 23:04:05','Y','2023-11-06 14:46:30','2023-11-04 16:31:56','2023-11-06 14:46:30');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -198,4 +232,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-03 20:32:51
+-- Dump completed on 2023-11-06 16:27:37
