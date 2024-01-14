ecc92142383bfe24c27197d785615c1028a9f3b0
siou <za96346@gmail.com>
2024-01-14 13:59:53 +0800

    delete: 刪除資料夾

--- microService/LoginService/main.go
+++
@@ -1,8 +0,0 @@
-package main
-//go get github.com/micro/micro/v2/cmd/protoc-gen-micro@master
-//go get -u github.com/golang/protobuf/protoc-gen-go
-//export PATH="$PATH:$(go env GOPATH)/bin"
-
-func main() {
-
-}
